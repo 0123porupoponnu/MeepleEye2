991da7015ae06df9850d060aad91f0e35f755556
0123porupoponnu <haruhi1126@gmail.com>
2026-08-29 00:01:11 +0900

    Refactor init array to include count and tile paths

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const init=[['修道院',tiles/tile_monastery.png],['道付修道院',tiles/tile_monastery_road.png],['ストレート',tiles/tile_straight.png],['カーブ',tiles/tile_curve.png],['三叉路',tiles/tile_crossroad.png],['十字路',tiles/tile_tjunction.png],['リップ',tiles/tile_city_cap.png],['アンダーバー',3],['隣接',2],['平行',3],['右折',3],['左折',3],['T字リップ',3],['土管',3],['2辺',5],['道付2辺',5],['3辺',4],['道付3辺',3],['4辺',1]];
+const init=[['修道院',4,'tiles/tile_monastery.png'],
+['道付修道院',2,'tiles/tile_monastery_road.png'],
+['ストレート',8,'tiles/tile_straight.png'],
+['カーブ',9,'tiles/tile_curve.png'],
+['三叉路',4,'tiles/tile_tjunction.png'],
+['十字路',1,'tiles/tile_crossroad.png'],
+['リップ',5,'tiles/tile_city_cap.png'],
+['アンダーバー',3,'tiles/tile_city_road.png'],
+['隣接',2,'tiles/tile_city_side.png'],
+['平行',3,'tiles/tile_city_parallel.png'],
+['右折',3,'tiles/tile11.png'],
+['左折',3,'tiles/tile12.png'],
+['T字リップ',3,'tiles/tile13.png'],
+['土管',3,'tiles/tile14.png'],
+['2辺',5,'tiles/tile15.png'],
+['道付2辺',5,'tiles/tile16.png'],
+['3辺',4,'tiles/tile17.png'],
+['道付3辺',3,'tiles/tile18.png'],                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 let tiles=init.map(x=>({...{name:x[0],count:x[1],start:x[1]}}));let history=[];
 const g=document.getElementById('grid'),t=document.getElementById('total');
 function draw(){g.innerHTML='';let total=0;tiles.forEach((x,i)=>{total+=x.count;const d=document.createElement('div');d.className='card'+(x.count===0?' zero':'');d.innerHTML=`<img src="tiles/tile${i+1}.png" onerror="this.src='meeple/icon-192.png'"><div class=n>${x.name}</div><div class=c>${x.count}</div>`;d.onclick=()=>{if(x.count>0){history.push(i);x.count--;draw();}};g.appendChild(d)});t.textContent=total}
